951820f696767dcf39e9042c7683bd134c715da8
Toms15 <info@tommasocostantini.it>
2021-11-10 19:00:11 +0100

    Fixed scripts and styles versioning

--- functions.php
+++ functions.php
@@ -142,10 +142,10 @@ add_action( 'widgets_init', 'wordpack_widgets_init' );
 function wordpack_scripts() {
 	wp_enqueue_style( 'wordpack-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'wordpack-style', 'rtl', 'replace' );
-	wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/css/app.css' );
+	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/css/app.css', array(), filemtime( get_template_directory() . '/css/app.css' ) );
 
 	wp_enqueue_script( 'wordpack-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-    wp_enqueue_script( 'custom-script', get_template_directory_uri() . '/js/app.js', array(), '1.0.0', true);
+    wp_enqueue_script( 'custom-script', get_template_directory_uri() . '/js/app.js', array(), filemtime( get_template_directory() . '/js/app.js' ), true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
