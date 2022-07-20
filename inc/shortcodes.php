61fe65603715fceb0a44d935c930efa6fe86d827
Toms15 <info@tommasocostantini.it>
2022-07-20 10:21:40 +0200

    feat: implement custom shortcodes

---
+++ inc/shortcodes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Sample implementation of the Shortcodes
+ *
+ * @package WordPack
+ */
+
+// add_shortcode('highlighted', function ($atts, $content = null) {
+//     extract(shortcode_atts([
+//         'color' => false,
+//     ], $atts));
+//     $classes = implode(' ', ['highlighted', $color ? 'tape--' . $color : '' ]);
+//     return sprintf('<span class="%s">%s</span>', $classes, $content);
+// });
