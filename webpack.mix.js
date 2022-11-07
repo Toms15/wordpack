283192418e4e09353471d938c2e2a729adb8005a
Toms15 <info@tommasocostantini.it>
2022-11-07 18:58:30 +0100

    Updated webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,26 +3,27 @@ const path = require('path');
 require('laravel-mix-purgecss');
 require('laravel-mix-webp');
 
-mix
-  .ImageWebp({
+mix.webpackConfig({
+    externals: {
+        // require("jquery") is external and available
+        //  on the global var jQuery
+        "jquery": "jQuery"
+    }
+});
+
+mix.ImageWebp({
     from: 'resources/images',
     to: 'images',
-  })
+})
 
 mix.js('resources/js/app.js', 'js')
-   .sass('resources/sass/app.scss', 'css')
-   .browserSync({
+    .sass('resources/sass/app.scss', 'css')
+    .browserSync({
        proxy: 'https://packword.test',
        files: [
               'css/**/*',
               'js/**/*'
        ],
-   })
-   .purgeCss({
-       extend: {
-         content: [
-            path.join(__dirname, '**/*.php')
-         ],
-         safelist: { deep: [/hljs/] },
-       },
-   });
+    });
+
+mix.minify('css/app.css');
